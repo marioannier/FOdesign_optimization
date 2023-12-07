fab02e476b012f376445df5019a9d259eecd18b3
marioannier <57014126+marioannier@users.noreply.github.com>
2023-12-07 14:43:14 +0100

    profile builder class finish

--- core_profile_index_builder.py
+++ core_profile_index_builder.py
@@ -3,7 +3,6 @@ import numpy as np
 import fiber_profile_gen as fp
 
 
-
 class ProfileIndexBuilder:
     def __init__(self, fimmap=object):
         # by default it generate a triangular a=1
@@ -16,53 +15,111 @@ class ProfileIndexBuilder:
         self.fimmap.Exec('app.addsubnode(fimmwave_prj,' + name + ')')
         self.fimmap.Exec('app.subnodes[1].savetofile(' + direc + ')')
 
-    def add_moduleFWG(self, name='FWG Waveguide 1', data_base='refbase.mat'):
+    def add_moduleFWG(self, name='FWG Waveguide 1', data_base='refbase_2.mat'):
         self.fimmap.Exec('app.subnodes[1].addsubnode(fwguideNode,' + name + ')')
         self.fimmap.Exec('app.subnodes[1].subnodes[1].setmaterbase("' + data_base + '")')
 
-    def builder_profile(self, dev, sizes, dop_perct, profile_type, materials, alpha):
-        n_steps = 100
+    def builder_profile(self, dev, sizes, dop_perct, profile_type, materials, alpha, n_steps=100):
+
+        # obtain the total number of layers
+        num_layers = self.obtain_num_layer(dev)
+
+        # Deleting all but first layer
+        while num_layers > 1:
+            self.fimmap.Exec(dev + ".deletelayer(" + str(num_layers) + ")")
+            num_layers = self.obtain_num_layer(dev)
 
         for i, type_regions in enumerate(profile_type):
+            # obtain the total number of layers
+            last_layer = self.obtain_num_layer(dev)
 
-            # creating the extra layers
-            num_layers = self.obtain_num_layer(dev)
-            if i > int(num_layers)-1:
-                self.fimmap.Exec(dev + ".insertlayer(" + str(i+1) + ")")
+            # creating the constant profile index layers
+            if type_regions == 'Contant':
+                if last_layer == 1 and i == 0:
+                    # rewriting the first layer
+                    current_layer = last_layer
+                    self.fimmap.Exec(dev + '.layers[1].cfseg=1')  # for measuring a certain parameters only in the core
+                else:
+                    current_layer = last_layer + 1
+                    self.fimmap.Exec(dev + '.insertlayer(' + str(current_layer) + ')')
+                self.fimmap.Exec(dev + '.layers[' + str(current_layer) + '].setMAT(' + materials[i] + ')')
+                self.fimmap.Exec(dev + '.layers[' + str(current_layer) + '].size=' + str(sizes[i]))
+
+                # for these cases, the dopant percentage is irrelevant
+                if "F-SiO2" not in materials[i]:
+                    self.fimmap.Exec(dev + '.layers[' + str(current_layer) + '].mx=' + str(dop_perct[i]))
 
             # creating the non-constant layers
-            if type_regions != 'contant':
+            else:
+                if "F-SiO2" in materials[i]:
+                    raise EnvironmentError("cannot be construct variable index profile with F-SiO2 at 1 or 2 %")
+
                 match type_regions:
-                    case "Linear":
-                        dop_perct_grad = fp.FiberProfileGen.graded_refindex(self, alpha, dop_perct[i], n_steps)
+                    # Triangular or Graded
+                    case "Linear" | "Graded":
+                        dop_perct_var = self.graded_refindex(alpha[i], dop_perct[i], n_steps)
 
-                    case "Graded":
-                         dop_perct_grad = fp.FiberProfileGen.graded_refindex(self, alpha, dop_perct[i], n_steps)
-
+                    # Raised cosine
                     case "Raised cosine":
-                        dop_perct_grad = fp.FiberProfileGen.rc_refindex(self, alpha, dop_perct[i], n_steps)
+                        dop_perct_var = self.rc_refindex(alpha[i], dop_perct[i], n_steps)
 
-                dop_perct.pop(0)
-                dop_perct = np.append(dop_perct_grad, dop_perct)
+                    case "Custome function":
+                        # working on
+                        pass
 
-                #### me quede
-
-
-
-
-            # +1 added 'cause fimmap start at 1
-            self.fimmap.Exec(dev + '.layers[' + str(i + 1) + '].setMAT(' + materials[i] + ')')
-            self.fimmap.Exec(dev + '.layers[' + str(i + 1) + '].size=' + str(sizes[i]))
-
-            # for these cases, the dopant percentage is irrelevant
-            if "F-SiO2" not in materials[i]:
-                self.fimmap.Exec(dev + '.layers[' + str(i + 1) + '].mx=' + str(dop_perct[i]))
-
-            # for measuring a certain parameters only in the core
-            if i == 1:
-                self.fimmap.Exec(dev + '.layers[' + str(i) + '].cfseg=1')
+              # loop to modify every layer
+                for numlayer in range(1, n_steps, 1):
+                    current_layer = last_layer + numlayer
+                    self.fimmap.Exec(dev + '.insertlayer(' + str(current_layer) + ')')
+                    self.fimmap.Exec(dev + '.layers[' + str(current_layer) + '].size=' + str(sizes[i] / n_steps))
+                    self.fimmap.Exec(dev + '.layers[' + str(current_layer) + '].setMAT(' + materials[i] + ')')
+                    self.fimmap.Exec(dev + ".layers[" + str(current_layer) + "].mx=" + str(dop_perct_var[numlayer - 1]))
+                    if last_layer == 1 and i == 0:
+                        self.fimmap.Exec(dev + '.layers[' + str(current_layer) + '].cfseg=1')
+                if last_layer == 1 and i == 0:
+                    # eliminating the first layer, it is the default
+                    self.fimmap.Exec(dev + ".deletelayer(1)")
 
     def obtain_num_layer(self, dev):
         # return a float with the number of layers
         num_layers = self.fimmap.Exec(dev + '.nlayers()')
         return num_layers
+
+    def rc_refindex(self, alpha, dopa_max, steps):
+        # based on : https://doi.org/10.1016/j.yofte.2021.102777
+        # Multicore raised cosine fibers for next generation space division multiplexing systems
+        # refractive index relation with GiO2-SiO2 percentage Percentage = -6.67677 * refractive_index + 9.64142
+        n1 = (dopa_max + 9.6495) / 6.6823
+        n2 = 9.6495 / 6.6823  # zero dopant n2 = 1.4440236217
+
+        y = np.zeros(steps)
+        perc = np.zeros(steps)
+
+        for j in range(steps):
+            if j <= ((1 - alpha) * (steps / 2)):
+                y[j] = n1
+            elif ((1 - alpha) * (steps / 2)) < j <= ((1 + alpha) * (steps / 2)):
+                y[j] = n2 + 0.5 * (n1 - n2) * (
+                        1 + np.cos(np.pi / (steps * alpha) * (j - steps * 0.5 * (1 - alpha))))
+            else:
+                y[j] = n2
+
+            perc[j] = 6.6823 * y[j] - 9.6495
+        return perc.round(4)
+
+    def graded_refindex(self, alpha, dopa_max, steps):
+        # refractive index relation with GiO2-SiO2 percentage Percentage = -6.67677 * refractive_index + 9.64142
+        n1 = (dopa_max + 9.6495) / 6.6823
+        n2 = 9.6495 / 6.6823  # zero dopant n2 = 1.4440236217
+
+        y = np.zeros(steps)
+        perc = np.zeros(steps)
+
+        delta = (n1 ** 2 - n2 ** 2) / (2 * n1 ** 2)
+
+        for j in range(steps):
+            y[j] = (n1 * np.sqrt(1 - 2 * delta * (j / steps) ** alpha))
+            perc[j] = 6.6823 * y[j] - 9.6495
+
+        return perc.round(4)
+
